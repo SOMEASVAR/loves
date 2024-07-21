9d1b5055cca2a5198a99ba47ac7c581f045670ff
Someasvar <deepakbakiya455@gmail.com>
2024-07-21 23:27:30 +0530

    first commit

---
+++ src/Landing.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Landing.css';
+
+
+const Landing=()=> {
+
+  return (
+    <>
+
+
+    <div class="hero">
+      
+      
+     
+     <div class="content">
+     
+      
+
+      <center>
+      
+  
+        <h1>Hello, I’m <span>SOM</span></h1>
+      </center>
+      </div>
+      </div> 
+
+     
+    <></>  
+      </>
+  );
+}
+export default Landing;
